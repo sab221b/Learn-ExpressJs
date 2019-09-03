5dfcc23de608215401ea5a23b10db9aaeebdc8c0
sab221b <sab221b@gmail.com>
2019-09-03 19:35:51 +0530

    add: auth route

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,33 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Profile = mongoose.model('Profile');
 const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local');
+
+exports.login = (req, res) => {
+    const userName = req.body.username;
+    const password = req.body.password;
+    if (req.body.username && req.body.password) {
+        User.findOne({ email: userName }, (err, user) => {
+            if (err) {
+                res.send(err);
+            } if (!user) {
+                res.send({ message: 'username is not registered' })
+            } if (user) {
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) {
+                        res.send(err)
+                    } if (isMatch) {
+                        res.send(user);
+                    } else {
+                        res.send({ message: 'password is incorrect' });
+                    }
+                })
+            }
+        })
+    } else {
+        res.send({ message: 'username and password is required' })
+    }
+};
 
 exports.getUsers = (req, res) => {
     if (req.query) {
@@ -30,7 +57,7 @@ exports.getUserById = (req, res) => {
     User.findById(req.params.id, (err, response) => {
         if (response) {
             response.populate('profile').execPopulate((err, data) => {
-                if(data) res.send(data);
+                if (data) res.send(data);
                 else res.send(err);
             });
         } else {
@@ -64,13 +91,12 @@ exports.deleteUser = (req, res) => {
 exports.createUser = (req, res) => {
     if (req.body.firstname && req.body.lastname && req.body.dob && req.body.email && req.body.mobile) {
         var user = new User();
-        user.username = req.body.username;
-        user.password = req.body.password;
         user.email = req.body.email;
+        user.password = req.body.password;
         user.mobile = req.body.mobile;
         User.find({ email: user.email }, (err, success) => {
             if (success.length) {
-                res.send({ message: "username already exists!" })
+                res.send({ message: "email already exists!" })
             } else if (!success.length && !err && user.password) {
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(user.password, salt, (err, hash) => {
@@ -87,7 +113,7 @@ exports.createUser = (req, res) => {
                                     user.save((err, response) => {
                                         if (response) {
                                             response.populate('profile').execPopulate((err, data) => {
-                                                if(data) res.send(data);
+                                                if (data) res.send(data);
                                                 else res.send(err);
                                             })
                                         } else {
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 app.use(cookieParser());
 const UserRoute = require('./routes/user');
+const AuthRoute = require('./routes/auth');
 
 const PORT = parseInt(process.env.PORT, 10) || 3000;
 app.use(session({ secret: "session" }));
@@ -14,17 +15,10 @@ app.use(bodyParser.json());
 
 app.get('/api/v1', (req, res) => {
     res.send({ message: 'Welcome to ExpressJS' })
-    // res.cookie('name', 'express-cookie', { expire: 3600 + Date.now() });
-    // if (req.session.page_views) {
-    //     req.session.page_views++;
-    //     res.send("You visited this page " + req.session.page_views + " times");
-    // } else {
-    //     req.session.page_views = 1;
-    //     res.send("Welcome to this page for the first time!");
-    // }
 });
 
 app.use('/api/v1/users', UserRoute);
+app.use('/api/v1/auth', AuthRoute);
 
 
 app.listen(PORT, () => {
